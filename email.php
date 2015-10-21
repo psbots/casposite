2ce0b219deed097002cff57d2a7df897964f338e
Praveen Sridhar <prvn431@gmail.com>
2015-10-21 23:19:23 +0530

    email

--- email.php
+++ email.php
@@ -1,14 +1,14 @@
 <?php
-$connection = mysql_connect("https://php-caspowebsite.rhcloud.com/phpmyadmin", "adminuECylHY", " FuJ8MUiwGikn");
-$db = mysql_select_db("email", $connection);
-if(isset($_GET['email'])){
+$connection = mysql_connect("50.112.134.215", "root", "isuck"); 
+$db = mysql_select_db("email", $connection); 
+if(isset($_GET['submit'])){ 
 
 $email = $_GET['email'];
 
 if($email !=''){
 //Insert Query of SQL
-$query = mysql_query("insert into email_id(email) values ('$email')");
-echo = "<p>Success</br></p>";
+$query = mysql_query("insert into email(email) values ('$email')");
+
 }
 else{
 echo "<p>Insertion Failed <br/></p>";
