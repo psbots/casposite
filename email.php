7c87e7d9a8594f0eed340661e64dd998a7cf0ed5
Praveen Sridhar <prvn431@gmail.com>
2015-10-21 23:28:06 +0530

    email

--- email.php
+++ email.php
@@ -1,12 +1,15 @@
+<html>
+<body>
+​
 <?php
 echo $_POST['email'];
 if (!empty($_POST)){
-
+​
 $servername = "127.5.71.130:24595";
 $username = "adminuECylHY";
 $password = "FuJ8MUiwGikn";
 $dbname = "email";
-
+​
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -14,16 +17,19 @@ if ($conn->connect_error) {
 die("Connection failed: ". $conn->connect_error);
 echo "error connecting db";
 }
-
+​
 $sql = "INSERT INTO email_id (email) VALUES ('".$_POST["email_name"]."')";
-
-
+​
+​
 if ($conn->query($sql) === TRUE) {
 echo '"New record created successfully"';
 } else {
 echo '"Error:"';
 }
-
+​
 $conn->close();
 }
 ?>
+​
+</body>
+</html>
