0c63e0783eadf731682ee0d3f670413ada1f2d72
Praveen Sridhar <prvn431@gmail.com>
2015-11-01 22:14:15 +0530

    vishnu gp

--- index.php
+++ index.php
@@ -8,64 +8,352 @@
 ?>
 
 
-<body id="single-while-bg no-overflow">
+<body id="top">
 
-    <section class="hero main-landing-page--img">
+    <section class="hero main-full-window single-page--hero--electric">
         <section class="navigation">
-  <?php
+           <?php
 
             //menu-elements
 
-            include('templates/menu_default.php');
+            include('templates/menu_temp.php');
 
             ?>
 
+             <div class="addition-nav">
+
+                <div class="a-nav--container">
+                    <a href="#design-showcase">Design</a>
+                    <a href="#feat-showcase">Features</a>
+                    <a href="#app-showcase">App</a>
+                </div>
+             </div>
         </section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-push-1">
+                    <div class="hero-content text-center">
+                        <h1>Your perfect cooking companion.</h1>
+                        <h4><font color="white">Coming soon on your favourite crowd-funding platform.</h4><br><br><br>
+                    </div>
+                   <div class="preorder-intro">
+                            <center><a href="#" class="btn btn-fill btn-large btn-margin-right pre-order-btn-intro">Subscribe now</a><br><br><h4><font color="white"> Subscribe now to get early bird discounts</font></h4></center>
+                    </div> -->
+                </div>
+
+
+<!--
+                <div class="col-md-6">
+                    <img class="hero-content img-responsive" src="img/caspo-cooktop1.jpg" />
+                </div>
+
+            -->
+            </div>
+        </div>
+        <div class="down-arrow floating-arrow"><a href="#"><i class="fa fa-angle-down"></i></a></div>
+    </section>
+
+    <div class="clear"></div>
+
+    <section class="features-extra section-padding" id="assets">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-push-1">
+                    <div class="feature-list text-center">
+                    <br>
+                        <h2>World's first learning stovetop.</h2><br>
+                        <p>Caspo learns from your cooking habits, understands your health constraints and helps you improve in the ways you want to improve. Caspo can help you reduce your energy bills, lose weight, gain muscles, eat tasty food even when you have diabetes, or reduce the amount of time you spend in front of your cooktop.
+                        </p>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <img align="right" src="imgs/centerized.412.jpg">
+ -->
+    </section>
+
+    <section class="image-show-slider section-padding text-center">
+        <div class="flexslider">
+            <ul class="slides">
+                <li class="ss1 slider-li">
+                    <div class="container">
+                        <h2>MULTITASK AWAY</h2>
+                        <p> Worry about your office presentation, your food will not be overcooked!The software and hardware works in synergy to learn your cooking habits and pattern. </p>
+                    </div>
+                </li>
+                <li class="ss2 slider-li"> <div class="container">
+                        <h2>CONTROL REMOTELY</h2>
+                        <p>Now control your stovetop remotely. Lesser physical effort means easier cooking for elderly and physically challenged</p>
+                    </div></li>
+                <li class="ss3 slider-li"> <div class="container">
+                        <h2>EAT HEALTHY</h2>
+                        <p>We track the Caspo health index of each member of your family and gives you personalized suggestions.</p>
+                    </div></li>
+            <ul>
+        </div>
+    </section>
+
+
+    <!--- Features -->
+    <section class="features-extra section-padding" id="assets">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5"> 
+                    <div class="feature-list">
+                        <h3><font color="#004078">Design</font></h3>
+                        <p>Caspo has an aesthetically pleasing design that is sleek and classy. It looks contemporary, bright and attractive. Being multifunctional, comfortable and stylish, it blends with the modern kitchen.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="macbook-wrap design--pic wp3"></div>
+        <div class="responsive-feature-img"><img src="imgs/renders/red.244.jpg" alt="responsive devices"></div>
+    </section>
+
+
+    <section class="features-extra section-padding feature--img-left" id="new-assets">
+
 
 
 
         <div class="container">
             <div class="row">
-                <div class="col-md-10 col-md-push-1">
-                    <div class="hero-content text-center">
-                        <h1>Cooking Made Simple.</h1>
-                        <p class="intro">World's first learning cooktop.</p>
-
-                        <div class="preorder-intro">
-                            <a href="#" class="btn btn-fill btn-large btn-margin-right pre-order-btn-intro">Pre-Order now</a>
-                        </div>
-                        <div id="mc_embed_signup">
-
-                            <div id="mc_embed_signup_scroll signup-form">
 
 
-                            <form>
-                            <input type="email" name="email" class="required email input-regular" placeholder="Enter your email address" id="mce-EMAIL" required pattern="[^ @]+@[^ @]+.[a-z]+">
 
-                             <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-fill sign-up-btn signup-intro">
-                           </form>
-                            <div id="mce-responses" class="clears">
-                                <div class="response" id="mce-error-response" style="display:none"></div>
-                                <div class="response" id="mce-success-response" style="display:none"></div>
-                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                            <div style="position: absolute; left: -5000px;"><input type="text" name="b_a3de5825e5d3f1ac1e10b36a6_df22f19834" tabindex="-1" value=""></div>
+                <div class="col-md-5 col-md-offset-7">
+                    <div class="feature-list">
+                        <h3><font color="#004078">Dual control knob</font></h3>
+                        <p>Ergonomically designed knob that controls flame and sets timer with minimal hand movement. Turn to adjust flame. Press and turn to set timer.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+
+        <div class="caspo-d1-wrap timer-nob--pic wp2"></div>
+        <div class="responsive-feature-img"><img src="imgs/renders/red.243.jpg" alt="responsive devices"></div>
+
+
+
+    </section>
+
+
+
+    <section class="features-extra section-padding" id="assets">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="feature-list">
+                        <h3><font color="#004078">Phone Docking</font></h3>
+                        <p>Ergonomically designed safe docking slot to allow browsing on your phone even while cooking. You can now have your recipes on display for easy cooking.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="macbook-wrap phone-doc--pic wp3"></div>
+        <div class="responsive-feature-img"><img src="imgs/renders/kitchen new 2.48.jpg" alt="responsive devices"></div>
+    </section>
+
+
+
+    <section class="features-extra section-padding" id="assets">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-md-offset-7">
+                    <div class="feature-list">
+                        <h3><font color="#004078">Temperature and Weight Sensor</font></h3>
+                        <p> Smart cooking powered by multiple sensors for that perfect cooking experience. This feature allows you to estimate the temperature of the dish and helps prevent overcooking. The weight sensor also helps to count your calorie intake like never before.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="caspo-d1-wrap temp-weight--pic wp1"></div>
+        <div class="responsive-feature-img"><img src="img/untitled.52.png" alt="responsive devices"></div>
+    </section>
+
+
+
+
+    <!---- End Features -->
+
+
+
+
+
+
+
+
+
+    <section class="intro section-padding" id="feat-showcase">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 intro-feature">
+                    <div class="intro-icon">
+                        <span class="icon"><img src="imgs/icons/smartphone20.png"></span>
+                    </div>
+                    <div class="intro-content">
+                        <h5>Phone Docking</h5>
+                        <p>Easy phone docking and access</p>
+                    </div>
+                </div>
+                <div class="col-md-4 intro-feature">
+                    <div class="intro-icon">
+                      <span class="icon"><img src="imgs/icons/view32.png"></span>
+                    </div>
+                    <div class="intro-content">
+                        <h5>Integrated Sensor</h5>
+                        <p>Temperature & weight sensor for precise cooking</p>
+                    </div>
+                </div>
+                <div class="col-md-4 intro-feature">
+                    <div class="intro-icon">
+                      <span class="icon"><img src="imgs/icons/buttons16.png"></span>
+                    </div>
+                    <div class="intro-content last">
+                        <h5>Light Indicated Timer / knob</h5>
+                        <p>Integrated timer & controls in a single knob</p>
+                    </div>
+                </div>
+
+
+                <!-- Fornt pic -->
+                <div class="col-md-12 intro-feature cooktop-mockup--front-display">
+                    <img src="imgs/renders/caspo-stove-front-view.jpg" class="img-responsive" />
+                </div>
+
+
+
+
+                <div class="col-md-4 intro-feature">
+                    <div class="intro-icon">
+                      <span class="icon"><img src="imgs/icons/flash31.png"></span>
+                    </div>
+                    <div class="intro-content">
+                        <h5>Electric / Induction</h5>
+                        <p>Available in induction & electric models</p>
+                    </div>
+                </div>
+                <div class="col-md-4 intro-feature">
+                    <div class="intro-icon">
+                      <span class="icon"><img src="imgs/icons/clean11.png"></span>
+                    </div>
+                    <div class="intro-content">
+                        <h5>Easy to clean</h5>
+                        <p>High quality glass for easy cleaning</p>
+                    </div>
+                </div>
+                <div class="col-md-4 intro-feature">
+                    <div class="intro-icon">
+                      <span class="icon"><img src="imgs/icons/computer190.png"></span>
+                    </div>
+                    <div class="intro-content last">
+                        <h5>Wifi connected.</h5>
+                        <p>Alway stay connected</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+            <section class="features section-padding" id="download">
+            <div class="container">
+                <div class="row">
+
+
+                    <div class="col-sm-6">
+                            <div class="mockup-nexus-a">
+                                <div class="mockup-nexus--display">
+
+                                    <div id="mobile-ui--slideshow">
+
+                                        <img src="imgs/ui/design-1--control.jpg" alt="" class="active" />
+
+                                        <img src="imgs/ui/design-1--cook.jpg" alt="" />
+
+                                        <img src="imgs/ui/design-1--pod.jpg" alt="" />
+
+
+                                    </div>
+
+
+                                </div>
                             </div>
-                        </div>
-
-
-
 
 
                     </div>
 
+
+
+
+
+                    <div class="col-md-6 col-xs-10 col-xs-offset-1 col-md-offset-0">
+                        <div class="app-ui-details">
+                            <div class="row">
+                                <ul id="mobile-ui--slideshow--desc">
+                                    <li class="active">
+                                        <h3>Control with your fingertips</h3>
+                                        <p>Now you can turn off the flame without leaving the comfort of your couch.This feature lets you control the flame pattern through the mobile app.No more missing out on the climax of your favorite TV shows.</p>
+                                    </li>
+
+                                    <li>
+                                        <h3> Recipe finder</h3>
+                                        <p>All the recipes of the world are a click-away from you. It is a must-have for the health conscious eaters who crave variety in their diet</p>
+                                    </li>
+
+                                    <li>
+                                        <h3>Tune in to your favourite podcasts</h3>
+                                        <p>Now you can enjoy podcasts from various foodies while you are cooking.There are a variety of podcasts that feature anything from tricks and tidbits to help you cook to mellifluous music to set the mood.</p>
+                                    </li>
+
+
+                                </ul>
+
+                                <a href="appshow.php" id="mc-embedded-subscribe" class="button btn btn-fill sign-up-btn signup-intro">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
+            </div>
+
+        </section>
 
 
+
+
+
+
+
+
+    <section class="parallax paralllex--design-1">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-push-3">
+                    <h3>Be among the early adopters</h3>
+                    <p>Help us continually improve Caspo and we will do as much as we can to satisfy your real needs. We have created an excellent functional prototype, but to scale to serial production we need your help.</p>
+
+                             <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-fill sign-up-btn signup-intro">
+                           </form>
+                </div>
             </div>
         </div>
 
-         <div class="footer-credits no-shadow">
+    </section>
+
+    <div class="clear"></div>
+        <div class="main-footer">
                     <?php
 
             //menu-elements
@@ -73,18 +361,76 @@
             include('templates/footer_default_credits.php');
 
         ?>
-    </div>
+        </div>
+
+
+
+
+
+<!--
+    <section class="features section-padding" id="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-md-offset-7">
+                    <div class="feature-list">
+                        <h3>Caspo helps you cook faster and smarter</h3>
+                        <p>Lorem ipsum dolor sit amet, no omnium tamquam posidonium ius, sale aperiri sadipscing nec ne, ei debitis dolorem mei.</p>
+                        <ul class="features-stack">
+                            <li class="feature-item">
+                                <div class="feature-icon">
+                                    <span data-icon="&#xe03e;" class="icon"></span>
+                                </div>
+                                <div class="feature-content">
+                                    <h5>Sambavam Adipoli ane</h5>
+                                    <p>Lorem ipsum dolor sit amet, no omnium tamquam posidonium ius.</p>
+                                </div>
+                            </li>
+                            <li class="feature-item">
+                                <div class="feature-icon">
+                                    <span data-icon="&#xe040;" class="icon"></span>
+                                </div>
+                                <div class="feature-content">
+                                    <h5>Freak ane</h5>
+                                    <p>Lorem ipsum dolor sit amet, no omnium tamquam posidonium ius. Lorem ipsum dolor sit amet, no omnium tamquam posidonium ius, </p>
+                                </div>
+                            </li>
+                            <li class="feature-item">
+                                <div class="feature-icon">
+                                    <span data-icon="&#xe03c;" class="icon"></span>
+                                </div>
+                                <div class="feature-content">
+                                    <h5>Pakka ane</h5>
+                                    <p>Lorem ipsum dolor sit amet, no omnium tamquam posidonium ius, Lorem ipsum dolor sit amet, no omnium tamquam posidonium ius, Lorem ipsum dolor sit amet, no omnium tamquam posidonium ius, </p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="device-showcase">
+            <div class="devices">
+                <div class="ipad-wrap wp1"></div>
+                <div class="iphone-wrap wp2"></div>
+            </div>
+        </div>
+        <div class="responsive-feature-img"><img src="img/devices.png" alt="responsive devices"></div>
     </section>
 
+-->
 
 
- <?php
+
+
+        <?php
 
             //menu-elements
 
             include('templates/footer_scripts.php');
 
         ?>
+
+
         <script>
             $(document).ready(function(){
                 // document.getElementById("mc-embedded-subscribe").disabled = true;
---
+++ templates/menu_temp.php
@@ -0,0 +1,33 @@
+<?php
+
+/***********************
+
+Default Menu
+
+********************/
+
+
+?>
+
+
+			<header>
+                <div class="header-content">
+                    <div class="logo"><a href="index.php"><img src="imgs/caspo-logo.png" width="100px" alt="Caspo logo"></a></div>
+                    
+                    <div class="navicon">
+                        <a class="nav-toggle" href="#"><span></span></a>
+                    </div>
+                </div>
+
+                
+
+            </header>
+
+
+<?php
+
+
+
+//End Menu list
+
+?>
